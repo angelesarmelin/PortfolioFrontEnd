f874ba462b2dc00a36f1e244b75e80150dde74e8
angelesarmelin <angelesarmelin97@gmail.com>
2022-11-17 14:29:33 -0300

    con jason

---
+++ jason/main.js
@@ -0,0 +1,9 @@
+const deportista= {
+    nombre:'Messi',
+    champion:4,
+
+};
+
+console.log(json.stringfy(deportista));
+console.log(json.parse(deportista));
+
---
+++ jason/main2.js
@@ -0,0 +1,1 @@
+fetch('file:///C:/Personal/Curso%20YoProgramo/angelesarmelin/PortfolioFrontEnd/jason/usuarios.json').then((response)=> response.json).then((usuario)=> console.log(usuario));
